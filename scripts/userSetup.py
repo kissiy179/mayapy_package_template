49426063df06a278947ad8e19f9f12d6071b0f84
kissiy179 <kissiy179@gmail.com>
2021-03-03 21:10:31 +0900

    test_commandsメニューを追加

---
+++ scripts/userSetup.py
@@ -0,0 +1,22 @@
+#encoding: utf-8
+import maya.utils
+import maya.cmds as cmds
+
+def show_message(message):
+    print(message)
+    cmds.inViewMessage(assistMessage=message,
+                        position='midCenter',
+                        fade=True,
+                        # fadeStayTime=3000,
+                        )
+
+
+# メニュー定義
+def create_menu():
+    cmds.setParent('MayaWindow')
+    cmds.menu(label='test commands', tearOff=True)
+    cmds.menuItem(l='test command 1', command='show_message("test command 1")')
+    cmds.menuItem(l='test command 2', command='show_message("test command 2")')
+
+# メニュー作成
+maya.utils.executeDeferred(create_menu)
